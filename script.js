f2c8fd3106d8d5ecd20b6a48c4807c2e727d210e
Pratishriya <115838471+Pratishriya@users.noreply.github.com>
2023-10-12 10:26:50 -0400

    shriya'sGallery

---
+++ script.js
@@ -0,0 +1,4 @@
+ // JavaScript to toggle the mobile menu
+ document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("menu").classList.toggle("active");
+});
